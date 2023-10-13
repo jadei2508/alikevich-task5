387a72bc85b1affd25f9dfadc6a9d82460785be9
Roman Alikevich <roma.alikevich8@gmail.com>
2023-10-13 14:59:38 +0300

    completed task 5

--- task5/ContentView.swift
+++ task5/ContentView.swift
@@ -8,19 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let colorArray: [Color] = [.white, .pink, .yellow, .black]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            VStack(alignment: .center, spacing: 0) {
+                ForEach((0...colorArray.count - 1), id: \.self) { i in
+                    Rectangle()
+                        .foregroundColor(colorArray[i])
+                }
+            }.ignoresSafeArea()
+                    GeometryReader { geometry in
+                        let view = Rectangle()
+                        view
+                            .frame(width: 80, height: 80)
+                            .cornerRadius(10)
+                            .position(x: geometry.size.width / 2,
+                                      y: geometry.size.height / 2)
+                            .addCustomBlendMode()
+                            
+                    }.draggable()
+        }.background(Color.clear).compositingGroup()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DraggableView: ViewModifier {
+    @State var offset = CGPoint(x: 0, y: 0)
+    
+    func body(content: Content) -> some View {
+        content
+            .gesture(DragGesture(minimumDistance: 0)
+                .onChanged { value in
+                    self.offset.x += value.location.x - value.startLocation.x
+                    self.offset.y += value.location.y - value.startLocation.y
+            })
+            .offset(x: offset.x, y: offset.y)
+    }
+}
+
+extension View {
+    func draggable() -> some View {
+        return modifier(DraggableView())
+    }
+}
+
+extension View {
+    func addCustomBlendMode() -> some View {
+        return modifier(CustomBlendMode())
+    }
+}
+
+struct CustomBlendMode: ViewModifier {
+    
+    let view: some View =  Rectangle()
+        .frame(width: 80, height: 80)
+        .cornerRadius(10)
+    
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.white)
+            .blendMode(.difference)
+            .overlay(content
+                .overlay(
+                    view
+                        .blendMode(.hue))
+                .overlay(
+                    view
+                        .foregroundColor(.white).blendMode(.overlay)
+                )
+                .overlay(
+                    view
+                        .foregroundColor(.black).blendMode(.overlay)
+                )
+                .blendMode(.overlay))
+            .overlay(content.foregroundColor(.white).blendMode(.overlay))
     }
 }
